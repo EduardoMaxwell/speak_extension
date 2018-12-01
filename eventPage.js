9f43fc55b87808f8b2934f8d2de388ab52a688bb
Eduardo Maxwell <eduardomaxwell2011@gmail.com>
2018-11-30 20:19:14 -0400

    fix the code

--- eventPage.js
+++ eventPage.js
@@ -9,6 +9,6 @@ chrome.contextMenus.create(menuItem);
 chrome.contextMenus.onClicked.addListener(function(clickData){
     if (clickData.menuItemId == "speak" && clickData.selectionText) {
         chrome.tts.speak(clickData.selectionText, {'lang': 'pt-BR','rate': 1.0});
-        chrome.tts.speak('Made by Eduardo Maxwell', {'lang': 'en-US','enqueue':true, 'rate': 2.0});
+        chrome.tts.speak('Made by Eduardo Maxwell', {'lang': 'en-US','enqueue':true, 'rate': 2.0, 'volume': 0.7});
     }
 });
